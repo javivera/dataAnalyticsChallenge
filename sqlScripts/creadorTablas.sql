c17a1f0d3fd6ab7f4ac40fb3a92bb2d5a560db09
Javier Vera <jvera.studio@gmail.com>
2022-04-29 18:46:06 -0300

    data loader

--- sqlScripts/creadorTablas.sql
+++ sqlScripts/creadorTablas.sql
@@ -1,4 +1,8 @@
-DROP TABLE IF EXISTS datos;
+DROP TABLE IF EXISTS datos_cines;
+DROP TABLE IF EXISTS datos_museos;
+DROP TABLE IF EXISTS datos_bibliotecas;
 DROP TABLE IF EXISTS cines;
 CREATE TABLE IF NOT EXISTS public.cines (provincia varchar, butacas integer, pantallas integer, espacio_incaa integer);
-CREATE TABLE IF NOT EXISTS public.datos (cod_localidad integer, id_provincia integer, id_departamento integer,categoria varchar, provincia varchar, localidad varchar, nombre varchar, domicilio varchar, codigo_postal varchar, num_telefono integer, mail varchar, web varchar);
+CREATE TABLE IF NOT EXISTS public.datos_cines (cod_localidad integer, id_provincia integer, id_departamento integer,categoria varchar, provincia varchar, localidad varchar, nombre varchar, domicilio varchar, codigo_postal varchar, numero_de_telefono integer, mail varchar, web varchar);
+CREATE TABLE IF NOT EXISTS public.datos_museos (cod_localidad integer, id_provincia integer, id_departamento integer,categoria varchar, provincia varchar, localidad varchar, nombre varchar, domicilio varchar, codigo_postal varchar, numero_de_telefono integer, mail varchar, web varchar);
+CREATE TABLE IF NOT EXISTS public.datos_bibliotecas (cod_localidad integer, id_provincia integer, id_departamento integer,categoria varchar, provincia varchar, localidad varchar, nombre varchar, domicilio varchar, codigo_postal varchar, numero_de_telefono integer, mail varchar, web varchar);
